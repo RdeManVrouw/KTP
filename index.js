24c207def5e24c531488520874f8794cf24d01f8
RdeManVrouw <r.de.man@student.rug.nl>
2023-12-06 08:20:05 +0100

    data is saved

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+var userData = {};
 
-
-
-
-
-
+function recieveInput(e){
+  var elem = e.target;
+  userData[elem.name] = elem.value;
+}
 
 var inputSlides = document.getElementsByClassName("inputSlide");
+for (var i = 0; i < inputSlides.length; i++){
+  for (var j = 0; j < inputSlides[i].children.length - 2; j++){
+    var child = inputSlides[i].children[j]; // a knowledgeElement div
+    child.className = "knowledgeElement";
+    child.children[0].className = "knowledgeElementTitle";
+    switch (child.children[1].tagName){
+      case "SELECT":
+        child.children[1].className = "selectInput";
+        break;
+      case "INPUT":
+        child.children[1].className = "textInput";
+    }
+    child.children[1].addEventListener("change", recieveInput);
+    child.children[1].name = child.children[0].innerHTML;
+    userData[child.children[0].innerHTML] = null;
+  }
+}
