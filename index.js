76d6ca1a9268c8383eddb65e278301aae834ce33
RdeManVrouw <r.de.man@student.rug.nl>
2023-12-07 08:35:35 +0100

    next and previous button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var slideIndex = 0;
+var inputSlides = document.getElementsByClassName("inputSlide");
 var userData = {};
 
 function recieveInput(e){
@@ -5,24 +7,51 @@ function recieveInput(e){
   userData[elem.name] = elem.value;
 }
 
-function nextButton(){
-  // validity check of the data
-  inputSlide[slideIndex].style.visibility = "hidden";
-  slideIndex++;
+function isDataValid(){
+  for (var i = 0; i < inputSlides[slideIndex].children.length; i++){
+    if (inputSlides[slideIndex].children[i].tagName != "DIV") continue;
+    var elem = inputSlides[slideIndex].children[i].children[1];
+    if (elem.tagName == "SELECT") continue;
+    var str = userData[elem.name];
+    if (str == null){
+      alert("Input required at '" + elem.name + "'");
+      return false;
+    }
+    switch (elem.name){
+      case "Date of Birth":
+        if (isNaN(new Date(str))){
+          alert("'Date of Birth' invalid");
+          return false;
+        }
+        break;
+      case "IBAN":
+        break;
+    }
+  }
+  return true;
+}
 
+function nextButton(){
+  if (!isDataValid()) return;
+  inputSlides[slideIndex].style.visibility = "hidden";
+  slideIndex++;
+  if (slideIndex >= inputSlides.length){
+    // initialize next stage
+  } else {
+    inputSlides[slideIndex].style.visibility = "visible";
+  }
 }
 
 function previousButton(){
-  // validity check of the data
-
+  inputSlides[slideIndex].style.visibility = "hidden";
+  slideIndex--;
+  inputSlides[slideIndex].style.visibility = "visible";
 }
 
-var slideIndex = 0;
-var inputSlides = document.getElementsByClassName("inputSlide");
 for (var i = 0; i < inputSlides.length; i++){
   if (i != slideIndex) inputSlides[i].style.visibility = "hidden";
-  var j;
-  for (j = 0; j < inputSlides[i].children.length - 2; j++){
+  for (var j = 0; j < inputSlides[i].children.length; j++){
+    if (inputSlides[i].children[j].tagName != "DIV") continue;
     var child = inputSlides[i].children[j]; // a knowledgeElement div
     child.className = "knowledgeElement";
     child.children[0].className = "knowledgeElementTitle";
@@ -37,6 +66,8 @@ for (var i = 0; i < inputSlides.length; i++){
     child.children[1].name = child.children[0].innerHTML;
     userData[child.children[0].innerHTML] = null;
   }
-  inputSlides[i].children[j].addEventListener("click", previousButton);
-  inputSlides[i].children[j + 1].addEventListener("click", nextButton);
 }
+var previousButtons = document.getElementsByClassName("previousButton");
+for (butn of previousButtons) butn.addEventListener("click", previousButton);
+var nextButtons = document.getElementsByClassName("nextButton");
+for (butn of nextButtons) butn.addEventListener("click", nextButton);
